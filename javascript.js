0f482280d48a170e52bd23a002bb6afb616fab61
Delta0S <156870537+Delta0S@users.noreply.github.com>
2024-04-27 10:17:29 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,22 +1,21 @@
 const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-const randomChar = () => chars[Math.floor(Math.random() * (chars.length - 1))],
-      randomString = length => Array.from(Array(length)).map(randomChar).join("");
+const randomChar = () => chars[Math.floor(Math.random() * chars.length)];
+const randomString = length => Array.from({ length }, () => randomChar()).join("");
 
-const card = document.querySelector(".card"),
-      letters = card.querySelector(".card-letters");
+const card = document.querySelector(".card");
+const letters = card.querySelector(".card-letters");
 
 const handleOnMove = e => {
-  const rect = card.getBoundingClientRect(),
-        x = e.clientX - rect.left,
-        y = e.clientY - rect.top;
+  const rect = card.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
 
   letters.style.setProperty("--x", `${x}px`);
   letters.style.setProperty("--y", `${y}px`);
 
   letters.innerText = randomString(1500);    
-}
+};
 
-card.onmousemove = e => handleOnMove(e);
-
-card.ontouchmove = e => handleOnMove(e.touches[0]);
+card.addEventListener("mousemove", handleOnMove);
+card.addEventListener("touchmove", e => handleOnMove(e.touches[0]));
